478b9ac21ab7820f87a28c0ac5c70a24991dc33c
Will Spaur <mitchellspaur@gmail.com>
2021-06-01 23:02:28 -0700

    full JS functionality and basic style applied

--- app.js
+++ app.js
@@ -18,25 +18,32 @@ Pseudo-code:
 
 First start out easy:
 
-Fetch info from API and find the info wanted
-First try in basic promises,
-then finalize in async/await
+	Fetch info from API and find the info wanted
+	First try in basic promises,
+	then finalize in async/await
 
-Create a search field to actively retrieve this info
+	Create a search field to actively retrieve this info
 
 Second:
 
-Use a Class
-Append this information into HTML and display correctly
-Toggle F/C
+	Use a Class
+	Append this information into HTML and display correctly
+	Toggle F/C
 
 Third: 
 
-Set corresponding styles and images for each weather type into their own sections
+	Weather conditions point => Image
+	If Error
+	Set corresponding styles and images for each weather type into their own sections
 Loading animation
 Make it look really good!
 
 */
+
+const leftForm = document.getElementById('left-form');
+const rightForm = document.getElementById('right-form');
+const leftDegree = document.getElementById('left-degree');
+const rightDegree = document.getElementById('right-degree');
 
 class Weather {
 	constructor(city, degree, container) {
@@ -53,7 +60,6 @@ class Weather {
 		} else {
 			setCity = newCity;
 		}
-		console.log(setCity);
 		this.city = setCity;
 	}
 
@@ -65,6 +71,15 @@ class Weather {
 		this.container = container;
 	}
 
+	handleError() {
+		const getDiv = document.getElementById(this.container);
+		getDiv.innerText = '';
+		const warning = document.createElement('h1');
+		warning.classList.add('cityH1');
+		warning.innerText = `Sorry couldn't find the city`;
+		getDiv.appendChild(warning);
+	}
+
 	async getWeather() {
 		const apiKey = 'f607d6cff65361bce0a424ea465c4eeb';
 		try {
@@ -74,8 +89,9 @@ class Weather {
 			);
 			const sorted = await response.json();
 			this.calculateNums(sorted);
+			console.log(sorted);
 		} catch (error) {
-			console.log(error);
+			this.handleError();
 		}
 	}
 
@@ -92,6 +108,7 @@ class Weather {
 		let windSpeed;
 		let windSpeedRating;
 		let humidity = data.main.humidity;
+		let weatherCond = data.weather[0].main;
 
 		let celsius = parseInt(data.main.temp - 273.15);
 		let countryDeterm = data.sys.country;
@@ -102,21 +119,21 @@ class Weather {
 			temp = Math.round(celsius);
 		}
 
-		if (rawDeg > 337 || rawDeg < 22) {
+		if (rawDeg > 337 && rawDeg < 22) {
 			windDirec = 'N';
-		} else if (rawDeg > 22 || rawDeg < 67) {
+		} else if (rawDeg > 22 && rawDeg < 67) {
 			windDirec = 'NE';
-		} else if (rawDeg > 67 || rawDeg < 112) {
+		} else if (rawDeg > 67 && rawDeg < 112) {
 			windDirec = 'E';
-		} else if (rawDeg > 112 || rawDeg < 157) {
+		} else if (rawDeg > 112 && rawDeg < 157) {
 			windDirec = 'SE';
-		} else if (rawDeg > 157 || rawDeg < 202) {
+		} else if (rawDeg > 157 && rawDeg < 202) {
 			windDirec = 'S';
-		} else if (rawDeg > 202 || rawDeg < 247) {
+		} else if (rawDeg > 202 && rawDeg < 247) {
 			windDirec = 'SW';
-		} else if (rawDeg > 247 || rawDeg < 297) {
+		} else if (rawDeg > 247 && rawDeg < 297) {
 			windDirec = 'W';
-		} else if (rawDeg > 297 || rawDeg < 337) {
+		} else if (rawDeg > 297 && rawDeg < 337) {
 			windDirec = 'NW';
 		}
 
@@ -135,7 +152,8 @@ class Weather {
 			windDirec,
 			windSpeed,
 			windSpeedRating,
-			humidity
+			humidity,
+			weatherCond
 		);
 	}
 
@@ -146,18 +164,35 @@ class Weather {
 		windDirec,
 		windSpeed,
 		windSpeedRating,
-		humidity
+		humidity,
+		weatherCond
 	) {
 		const getDiv = document.getElementById(this.container);
+		getDiv.innerText = '';
 
 		const cityH1 = document.createElement('h1');
 		cityH1.classList.add('locations');
 		cityH1.innerText = city;
 		getDiv.appendChild(cityH1);
 
+		const weatherPic = document.createElement('img');
+		weatherPic.classList.add('imgs');
+		if (weatherCond === 'Clear') {
+			weatherPic.src = 'imgs/sun.png';
+		} else if (weatherCond === 'Clouds') {
+			weatherPic.src = 'imgs/cloudy.png';
+		} else if (weatherCond === 'Rainy') {
+			weatherPic.src = 'imgs/rainy.png';
+		} else if (weatherCond === 'Snow') {
+			weatherPic.src = 'imgs/snowy.png';
+		} else {
+			weatherPic.src = 'imgs/summer.png';
+		}
+		getDiv.appendChild(weatherPic);
+
 		const tempH2 = document.createElement('h2');
 		tempH2.classList.add('temperatures');
-		tempH2.innerText = `Temperature: ${temp} ${this.degree}`;
+		tempH2.innerText = `${temp}° ${this.degree}`;
 		getDiv.appendChild(tempH2);
 
 		const weatherH3 = document.createElement('h3');
@@ -177,8 +212,68 @@ class Weather {
 	}
 }
 
-const firstCity = new Weather();
-firstCity.setCity = 'Yamagata';
-firstCity.setDegree = 'C';
-firstCity.setContainer = 'container2';
-firstCity.getWeather();
+leftForm.addEventListener('submit', e => {
+	e.preventDefault();
+	const city = document.getElementById('left-search').value;
+	const firstCity = new Weather();
+	firstCity.setCity = city;
+	firstCity.setDegree = leftDegree.innerText;
+	firstCity.setContainer = 'container1';
+	firstCity.getWeather();
+	leftForm.reset();
+});
+
+rightForm.addEventListener('submit', e => {
+	e.preventDefault();
+	const city = document.getElementById('right-search').value;
+	const secondCity = new Weather();
+	secondCity.setCity = city;
+	secondCity.setDegree = rightDegree.innerText;
+	secondCity.setContainer = 'container2';
+	secondCity.getWeather();
+	rightForm.reset();
+});
+
+leftDegree.addEventListener('click', e => {
+	if (leftDegree.innerText === 'F') {
+		leftDegree.innerText = 'C';
+		leftDegree.classList.remove('F');
+		leftDegree.classList.add('C');
+	} else if (leftDegree.innerText === 'C') {
+		leftDegree.innerText = 'F';
+		leftDegree.classList.remove('C');
+		leftDegree.classList.add('F');
+	}
+});
+
+rightDegree.addEventListener('click', e => {
+	if (rightDegree.innerText === 'F') {
+		rightDegree.innerText = 'C';
+		rightDegree.classList.remove('F');
+		rightDegree.classList.add('C');
+	} else if (rightDegree.innerText === 'C') {
+		rightDegree.innerText = 'F';
+		rightDegree.classList.remove('C');
+		rightDegree.classList.add('F');
+	}
+});
+
+function startUp() {
+	leftDegree.innerText = 'C';
+	leftDegree.classList.add('C');
+	const firstCity = new Weather();
+	firstCity.setCity = 'Yamagata';
+	firstCity.setDegree = leftDegree.innerText;
+	firstCity.setContainer = 'container1';
+	firstCity.getWeather();
+
+	rightDegree.innerText = 'F';
+	rightDegree.classList.add('F');
+	const secondCity = new Weather();
+	secondCity.setCity = 'Las Vegas';
+	secondCity.setDegree = rightDegree.innerText;
+	secondCity.setContainer = 'container2';
+	secondCity.getWeather();
+}
+
+startUp();
